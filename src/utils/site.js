bbb7e3f5b18a60bf35e3c9e3dba30bf9d05eb21e
pujinuning <163517455+pujinuning@users.noreply.github.com>
2025-07-12 13:52:00 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -4,6 +4,6 @@ export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Colmek Smp Vi
 export const terbit = '2025-02-17T00:03:13Z';
 
 export const google = 'f9vWuSKgqYxweDRBXKOzIZX8v4CcyHC_qK0bfOO6oAo';
-export const yandex = '7a8c2527b2e47214'
+export const yandex = '8a76e150fb90d891'
 
 export const index = 'e092d1a5-7c0c-48f7-a281-7b2dcd19d79b';
